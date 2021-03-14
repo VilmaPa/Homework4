1a2042768e0f82cc1aabacbdefed88d350840a94
VilmaPa <70938111+VilmaPa@users.noreply.github.com>
2021-03-14 23:14:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            ScrapingBrowser browser = new ScrapingBrowser();
+
+            for (int i = 1; i <= 5; i++)
+            {
+
+                WebPage homePage = browser.NavigateToPage(new Uri("https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos?page=" + i));
+
+                var html = homePage.Html;
+
+                var nodes = html.CssSelect(".cvo_module_offer h2 a");
+
+                var professionNames = nodes.Where(n => n.InnerText.Contains(".NET")).Select(n => n.InnerText);
+
+                foreach (var name in professionNames)
+                {
+                    System.Console.WriteLine(name);
+                }
+            }
+         
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# C# homework
+
+## Welcome! 👋
+
+This was coding project in C#.
+
+
+## The goal of this project
+
+- WebScraper. Job description from CVonline.lt 5 first pages.
+
+#Author
+
+[Vilma](https://github.com/VilmaPa)
